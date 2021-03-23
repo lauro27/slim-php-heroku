e26051c9156188cca7541759a402978747e00008
Juan Pablo Lamas <Lauro-JP@hotmail.com>
2021-03-23 20:20:45 -0300

    Ejercicio 11 arreglado

--- app/ejercicio11/index.php
+++ app/ejercicio11/index.php
@@ -2,12 +2,18 @@
 /*
 Lamas Juan Pablo
 Aplicación No 11 (Potencias de números)
+Mostrar por pantalla las primeras 4 potencias de los números del uno 1 al 4 (hacer una función
+que las calcule invocando la función pow).
 */
-
-for ($i=1; $i <= 4; $i++) { 
-    for ($j=1; $j <= 4; $j++) { 
-        echo "$i^$j: " . pow($i,$j) . "<br/>";
+function Potencias(){
+    for ($i=1; $i <= 4; $i++) { 
+        for ($j=1; $j <= 4; $j++) { 
+            echo "$i^$j: " . pow($i,$j) . "<br/>";
+        }
     }
+    
 }
+
+Potencias();
 
 ?>
--- app/index.php
+++ app/index.php
@@ -2,7 +2,7 @@
 $ejerciciosResueltos = 11;
 
 for ($i=11; $i <= $ejerciciosResueltos; $i++) { 
-    echo "<a href:\"/ejercicio". $i ."/index.php\">Ejercicio ".$i."</a>";
+    print("<a href:\"\/ejercicio". $i ."\/index.php\">Ejercicio ".$i."</a>");
 }
 
 ?>
